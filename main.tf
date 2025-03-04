fb0cb6302c7a4a9cffad16efebf31d77c205b8a3
Raghuadavi <126375262+Raghuadavi@users.noreply.github.com>
2025-03-04 10:37:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,4 +17,9 @@ resource "google_compute_instance" "default" {
   name         = "my-instance"
   machine_type = "n2-standard-2"
   zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
 }
