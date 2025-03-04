4c39586f88f56c0072ad136414c2b041d2587bd4
Raghuadavi <126375262+Raghuadavi@users.noreply.github.com>
2025-03-04 10:50:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,24 +13,16 @@ resource "google_storage_bucket" "my_bucket" {
   name     = "my-terraform-bucket-${random_id.bucket_suffix.hex}"
   location = "US"
 }
-resource "google_compute_instance" "default" {
-  name         = "my-instance"
-  machine_type = "n2-standard-2"
-  zone         = "us-central1-a"
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
+resource "google_compute_firewall" "http-server" {
+  name    = "${var.prefix}-default-allow-ssh-http"
+  network = google_compute_network.hashicat.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80"]
   }
+
+  // Allow traffic from everywhere to instances with an http-server tag
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
 }
-  scratch_disk {
-    interface = "NVME"
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
